76c191004415a2501c23233ce3b65a00cc330cf8
Aditi Khandelwal <46483537+AditiKhandelwal@users.noreply.github.com>
2020-09-24 21:58:45 +0530

    Update script.js

--- script.js
+++ script.js
@@ -6,6 +6,7 @@ const movieSelect = document.getElementById('movie');
 
 populateUI();
 
+// + is used to change the type of value from string to number
 let ticketPrice = +movieSelect.value;
 
 //Save selected movie index and price in local storage
@@ -20,10 +21,21 @@ function updateSelectedCount(){
     const selectedSeats =  document.querySelectorAll('.row .seat.selected');
     
     // storing index of selected seats
-    const seatsIndex = [...selectedSeats].map(seat => [...seats].indexOf(seat));
+    //  gives a nodelist-convert to array using spread operator and map throgh this
+    //COPY SLECTED SEATS IN AN ARRAY
+    //MAP THROUGH ARRAY
+    //RETURN A NEW ARRAY INDEXES
+    const seatsIndex = [...selectedSeats].map(
+        //find index of selected seat passed as input from all seats
+        seat => [...seats].indexOf(seat));
 
     //saving selected seat index
+    //key,value pair both are string and seatsIndex is an array therefore wrap in stringify to make it a string
    localStorage.setItem('selectedSeats', JSON.stringify(seatsIndex));
+    
+   //local storage-save the selected seats such that on refreshing page they are not  lost
+  //selected seat returns a nodelist of all same elements thus we store the indexes in an array
+  
    
     // updating price and no of seats
     const selectedSeatsCount = selectedSeats.length;
@@ -34,7 +46,9 @@ function updateSelectedCount(){
 //Get data from local storage and populate UI
 function populateUI(){
     // get previous selected seats index
+    //we get it in string form,want it back in an array therefore we parse it
     const selectedSeats =  JSON.parse(localStorage.getItem('selectedSeats'));
+    //now if there are seats in the local storage we wanna loop through them and add selected class to them
     if(selectedSeats !== null && selectedSeats.length>0)
     {
         seats.forEach((seat, index) => {
@@ -53,6 +67,7 @@ function populateUI(){
 }
 
 //Seat click event
+// we can also add event listener to each seat by using for each but using on ontainer is easier
 container.addEventListener("click", function (e){
     // if clicked on seat which is not occupied than toggle selected class
  if(e.target.classList.contains('seat') && !e.target.classList.contains('occupied')){
